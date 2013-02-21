d3216f930254a09c14abcc0830d421f15149fba4
geffzhang <geffzhang@qq.com>
2013-02-21 09:43:01 +0800

    Revert "delete quartz web service"
    This reverts commit e581fc6c4a3e526faa16173c47630ddbfc14cc7d.

---
+++ src/CrystalQuartz.Web/GroupStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CrystalQuartz.Web
+{
+    public class GroupStatus
+    {
+        public string Group { get; set; }
+
+        public bool IsJobGroupPaused { get; set; }
+
+        public bool IsTriggerGroupPaused { get; set; }
+    }
+}
